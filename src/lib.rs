a46eb38d04defdbbdbeb0ded48ffd260f0f417c1
woodyZootopia <piequalsabout314159@gmail.com>
2020-04-17 23:34:02 +0900

    inputs are also passed as reference.

--- src/lib.rs
+++ src/lib.rs
@@ -13,8 +13,8 @@ pub mod rsh {
             print!("> ");
             io::stdout().flush().unwrap(); // make sure above `> ` is printed
             let line = rsh_read_line();
-            let args = rsh_split_line(&line);
-            match rsh_execute(args, &available_binaries) {
+            let config = rsh_split_line(&line);
+            match rsh_execute(&config, &available_binaries) {
                 Some(Status::Exit) => break,
                 _ => (),
             }
@@ -56,17 +56,17 @@ pub mod rsh {
         Exit,
     }
 
-    fn rsh_execute(config: CommandConfig, available_binaries: &HashMap<OsString,String>) -> Option<Status> {
+    fn rsh_execute(config: &CommandConfig, available_binaries: &HashMap<OsString,String>) -> Option<Status> {
         match config.command {
-            Some("cd") => rsh_cd(config.args),
-            Some("help") => rsh_help(config.args),
-            Some("exit") => rsh_exit(config.args),
-            Some("pwd") => rsh_pwd(config.args),
+            Some("cd") => rsh_cd(&config.args),
+            Some("help") => rsh_help(&config.args),
+            Some("exit") => rsh_exit(&config.args),
+            Some("pwd") => rsh_pwd(&config.args),
             _ => rsh_launch(config, &available_binaries),
         }
     }
 
-    fn rsh_launch(_config: CommandConfig, available_binaries: &HashMap<OsString,String>) -> Option<Status> {
+    fn rsh_launch(_config: &CommandConfig, available_binaries: &HashMap<OsString,String>) -> Option<Status> {
         match fork() {
             Ok(ForkResult::Parent { child, .. }) => {
                 // parent
@@ -99,7 +99,7 @@ pub mod rsh {
         );
     }
 
-    fn rsh_cd(args: Vec<&str>) -> Option<Status> {
+    fn rsh_cd(args: &Vec<&str>) -> Option<Status> {
         assert!(
             args.len() > 0,
             "going to home directory just by `cd` is not supported for now"
@@ -110,18 +110,18 @@ pub mod rsh {
         Some(Status::Success)
     }
 
-    fn rsh_help(_args: Vec<&str>) -> Option<Status> {
+    fn rsh_help(_args: &Vec<&str>) -> Option<Status> {
         println!("Woody's re-implemantation of lsh, written in Rust.",);
         println!("Type command and arguments, and hit enter.",);
         // println!("The following commands are built in:",);
         Some(Status::Success)
     }
 
-    fn rsh_exit(_args: Vec<&str>) -> Option<Status> {
+    fn rsh_exit(_args: &Vec<&str>) -> Option<Status> {
         Some(Status::Exit)
     }
 
-    fn rsh_pwd(_args: Vec<&str>) -> Option<Status> {
+    fn rsh_pwd(_args: &Vec<&str>) -> Option<Status> {
         println!("{:?}", getcwd().unwrap());
         Some(Status::Success)
     }
